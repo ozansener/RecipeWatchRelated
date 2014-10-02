9db6fc5d7a747dc12e98b3e7ddae51cb36f6b096
Jay Hack <jhack@stanford.edu>
2014-10-02 11:34:20 -0700

    StorageDelegate updated to return JSON from mongodb

--- src/RecipeWatchRelated/StorageDelegate.py
+++ src/RecipeWatchRelated/StorageDelegate.py
@@ -12,6 +12,7 @@ import sys
 import random
 import pandas as pd
 import numpy as np
+from pymongo import MongoClient
 from Video import *
 
 
@@ -22,26 +23,12 @@ class StorageDelegate:
 	######################[ --- INTERNALS --- ]#########################################################
 	####################################################################################################
 
-	def __init__ (self, data_dir=os.path.join(os.environ['PROJECT_DIR'], 'data/cpmc/CPMC')):
-		self.data_dir = data_dir
-		self.video_dirs = self.get_video_dirs(self.data_dir)
-		self.videos = self.get_videos(self.video_dirs)
-
-
-	def get_video_dirs(self, data_dir):
-		"""
-			given cpmc_dir, returns list of full paths to individual dirs
-		"""
-		return {d:os.path.join(data_dir, d) for d in os.listdir(data_dir) if not d.startswith('.')}
-
-
-	def get_videos(self, video_dirs):
-		"""
-			returns dict: VideoName -> Video
-		"""
-		return {name:Video(name, video_dir) for name, video_dir in video_dirs.items()}
-
-
+	def __init__ (self):
+		
+		#=====[ Setup DB	]=====
+		self.mongo_client = MongoClient()
+		self.db = self.mongo_client.RecipeWatchDB
+		self.videos = self.db.videos
 
 
 	####################################################################################################
@@ -53,17 +40,14 @@ class StorageDelegate:
 		"""
 			returns the named video (unloaded)
 		""" 
-		if not video_name in self.videos.keys():
-			raise Exception("No video named %s in listed videos")
-		else:
-			return self.videos[video_name]
+		return self.videos.find_one(spec_or_id=video_name)
 
 
 	def get_random_video(self):
 		"""
 			returns a random video (unloaded)
 		"""
-		return self.get_video(random.choice(self.videos.keys()))
+		return self.videos.find_one()
 
 
 	

ec586ba725effbd817ab3f4b4a28691c3bd71334
Jay Hack <jhack@stanford.edu>
2014-10-21 17:45:38 -0700

    run configure.sh and you should be able to import caffe

--- configure.sh
+++ configure.sh
@@ -1,4 +1,11 @@
-export PYTHONPATH=$PYTHONPATH:`pwd`/src/
 export PROJECT_DIR=`pwd`
 export MONGODB_DBPATH=`pwd`/data/db
 export JAY_DATA_PATH=/Volumes/DeepStorage/Data/Robobrain/data
+export PYTHONPATH=$PYTHONPATH:`pwd`/src/
+
+#===[ Add Caffe to Path ]===
+export CAFFE_ROOT_PATH=/Users/jayhack/CS/CV/caffe
+export PYTHONPATH=$PYTHONPATH:$CAFFE_ROOT_PATH/python
+
+#===[ Add DYLD_FALLBACK_LIBRARY_PATH So Caffe can find .dyld libraries ]===
+export DYLD_FALLBACK_LIBRARY_PATH=/usr/local/cuda/lib:/usr/local/Cellar/hdf5/1.8.13/lib/:/usr/local/cuda/lib:/usr/local/Cellar/hdf5/1.8.13/lib/:/usr/local/cuda/lib:/Users/jayhack/anaconda/lib:/usr/local/lib:/usr/lib

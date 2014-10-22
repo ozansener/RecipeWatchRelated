6126d59491c97e12afdc7b8c86e78f14c4cc6f28
Jay Hack <jhack@stanford.edu>
2014-10-21 19:32:52 -0700

    updated __init__ so that including all includes CaffeCNN

--- src/RecipeWatchRelated/__init__.py
+++ src/RecipeWatchRelated/__init__.py
@@ -1,4 +1,5 @@
-__all__ = ['StorageDelegate', 'Video', 'Frame']
+__all__ = ['StorageDelegate', 'Video', 'Frame', 'CaffeCNN']
 from RecipeWatchRelated.StorageDelegate import StorageDelegate
 from RecipeWatchRelated.Video import Video
 from RecipeWatchRelated.Frame import Frame
+from RecipeWatchRelated.CaffeCNN import CaffeCNN

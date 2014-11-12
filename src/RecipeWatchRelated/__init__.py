d4b8163f119d2ddd265e4c0c1902d1406fcfcce3
Jay Hack <jhack@stanford.edu>
2014-11-11 18:58:36 -0800

    beginning integrating with new ModalDB

--- src/RecipeWatchRelated/__init__.py
+++
@@ -1,6 +0,0 @@
-__all__ = ['StorageDelegate', 'Video', 'Frame', 'CaffeCNN']
-from RecipeWatchRelated.StorageDelegate import StorageDelegate
-from RecipeWatchRelated.Video import Video
-from RecipeWatchRelated.Frame import Frame
-from RecipeWatchRelated.CaffeCNN import CaffeCNN
-# from RecipeWatchRelated.visualization_utils import load_cluster_data, visualize_cluster_sample
